467c71ae7b495a0d62a9fd4ebd17c013b72a14bd
viktoria.koloda@binary-studio.com <viktoria.koloda@binary-studio.com>
2024-06-16 21:04:56 +0200

    add supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://dwxllzuqsyrlsysbppgl.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3eGxsenVxc3lybHN5c2JwcGdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg1NjIyOTgsImV4cCI6MjAzNDEzODI5OH0.nAxXJOF3qrXLZWX1by7TheAUPPi10xPHFJGvA6QsaFs';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
