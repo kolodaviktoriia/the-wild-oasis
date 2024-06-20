cd2c1f966f5b54eb9b47bb13a3e25c26e6a60697
viktoria.koloda@binary-studio.com <viktoria.koloda@binary-studio.com>
2024-06-21 00:57:20 +0200

    add context menu

--- src/features/cabins/CabinRow-v1.jsx
+++ src/features/cabins/CabinRow-v1.jsx
@@ -89,28 +89,20 @@ function CabinRow({ cabin }) {
           <button disabled={isCreating}></button>
 
           <Modal>
-            <Menus.Menu>
-              <Menus.Toggle id={cabinId} />
-              <Menus.List id={cabinId}>
-                <Menus.Button
-                  icon={<HiSquare2Stack />}
-                  onClick={handleDuplicate}
-                >
-                  Duplicate
-                </Menus.Button>
-                <Modal.Open opens='edit'>
-                  <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
-                </Modal.Open>
-                <Modal.Open opens='delete'>
-                  <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
-                </Modal.Open>
-              </Menus.List>
-            </Menus.Menu>
-
+            <Modal.Open opens='edit'>
+              <button>
+                <HiPencil />
+              </button>
+            </Modal.Open>
             <Modal.Window name='edit'>
               <CreateCabinForm cabinToEdit={cabin} />
             </Modal.Window>
 
+            <Modal.Open opens='delete'>
+              <button disabled={isDeleting}>
+                <HiTrash />
+              </button>
+            </Modal.Open>
             <Modal.Window name='delete'>
               <ConfirmDelete
                 resourceName='cabins'
