3eea541f5e257eecb7a06b391c2920f7282c211b
viktoria.koloda@binary-studio.com <viktoria.koloda@binary-studio.com>
2024-06-21 18:00:39 +0200

    add sort to table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -14,6 +14,7 @@ function CabinTable() {
 
   if (isLoading) return <Spinner />;
 
+  //1.Filter
   const filterValue = searchParams.get('discount') || 'all';
 
   let filteredCabins;
@@ -23,6 +24,16 @@ function CabinTable() {
 
   if (filterValue === 'with-discount')
     filteredCabins = cabins.filter((cabin) => cabin.discount !== 0);
+
+  //2.Sort
+
+  const sortBy = searchParams.get('sortBy') || 'startDate-asc';
+
+  const [field, direction] = sortBy?.split('-');
+  const modifier = direction === 'asc' ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
@@ -36,7 +47,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
