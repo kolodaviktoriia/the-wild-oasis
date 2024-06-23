20350c6f2f692666fc83b8aa784e33c324b72138
viktoria.koloda@binary-studio.com <viktoria.koloda@binary-studio.com>
2024-06-24 00:05:26 +0200

    fix bug

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -9,7 +9,7 @@ export function useLogin() {
   const { isLoading, mutate: login } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      queryClient.setQueryData(['user'], user);
+      queryClient.setQueryData(['user'], user.user);
       navigate('/dashboard', { replace: true });
     },
     onError: (err) => {
